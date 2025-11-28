a71e5e078ca2c2945ca685c40709166fa0437e9e
mikocst <mikocastillo7@gmail.com>
2025-11-28 01:00:45 -0600

    Refactor Button component and enhance styles; add hero copy to index page; update Tailwind config for new colors and font

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,13 +6,13 @@ interface ButtonProps {
 }
 
 const Button = ({buttonCopy}: ButtonProps) => {
-    const MotionButton = motion('button');
+    const MotionButton = motion.create('button');
     return (
         <MotionButton
-            whileHover={{ scale: 1.1, transition: { duration: 0.1 } }}
+            whileHover={{ scale: 1.1, transition: { duration: 0.1 }, backgroundColor: '#0f327c' }}
             whileTap={{ scale: 0.9 }}
             transition={{ duration: 0.1 }}
-            className="px-4 py-2 bg-blue-500 text-white rounded-lg"
+            className="px-4 py-2 bg-primary text-white rounded-md"
         >
             {buttonCopy}
         </MotionButton>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx,astro}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#133F9C',
+        primaryLight: '#2a52a5 ',
+        primaryBright: '#4265af',
+        primaryDark: '#11388c',
+        primaryDarker: '#0f327c',
+        secondary: "#030c1c"
+      },
+      fontFamily: {
+        sans: ["Space Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };

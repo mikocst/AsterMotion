e2d6a3837c50abf15fbd3c63daa1caf5ab2b4409
mikocst <mikocastillo7@gmail.com>
2025-11-28 01:25:45 -0600

    Enhance Button component with variant support and update index page to utilize button variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,28 @@
 import {motion} from 'motion/react';
-import { buttonCopy } from '../types/types';
+import { buttonCopy, buttonVariant } from '@types';
 
 interface ButtonProps {
     buttonCopy: buttonCopy;
+    variant: buttonVariant;
 }
 
-const Button = ({buttonCopy}: ButtonProps) => {
+const Button = ({buttonCopy, variant}: ButtonProps) => {
     const MotionButton = motion.create('button');
+    const variantStyles = {
+        [buttonVariant.Primary]: 'bg-primary text-white',
+        [buttonVariant.Secondary]: 'bg-gray-50 border border-gray-100 border-primary text-primary'
+    };
+
+    const hoverVariants = {
+        [buttonVariant.Primary]: { backgroundColor: '#0f327c' },
+        [buttonVariant.Secondary]: { backgroundColor: '#f3f4f6', borderColor: '#e5e7eb' }
+    }
     return (
         <MotionButton
-            whileHover={{ scale: 1.1, transition: { duration: 0.1 }, backgroundColor: '#0f327c' }}
+            whileHover={hoverVariants[variant]}
             whileTap={{ scale: 0.9 }}
             transition={{ duration: 0.1 }}
-            className="px-4 py-2 bg-primary text-white rounded-md"
+            className={`px-4 py-2 rounded-md font-medium ${variantStyles[variant]}`}
         >
             {buttonCopy}
         </MotionButton>
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,8 @@ export enum buttonCopy {
     CLICK_ME = "Click Me",
     COPY_CODE = "Copy Code"
 }
+
+export enum buttonVariant {
+    Primary = 1,
+    Secondary = 2
+}

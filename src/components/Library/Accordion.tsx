75e2b2f944702bd3cc441bfb306e74a367b693ff
mikocst <mikocastillo7@gmail.com>
2026-01-26 15:51:35 -0600

    polished accordion component adding accessibility.

--- src/components/Library/Accordion.tsx
+++ src/components/Library/Accordion.tsx
@@ -1,29 +1,22 @@
 import { ChevronLeft } from "lucide-react";
 import {AnimatePresence, motion} from 'motion/react';
-import { useState } from "react";
+import { useState, useId } from "react";
 
-const accordionItems = [
-    {
-        title: "Product",
-        content: "Product content goes here."
-    },
-    {
-        title: "Design",
-        content: "Design content goes here."
-    },
-    {
-        title: "Development",
-        content: "Development content goes here."
-    }
-]
+interface AccordionProps {
+    items: Array<{
+        name: string
+        content: string
+    }>
+}
 
 const MotionDiv = motion.create('div');
 const MotionChevronLeft = motion.create(ChevronLeft);
 
-const Accordion = () => {
+const Accordion = ({ items}: AccordionProps) => {
+  const generatedId = useId();
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
-  const toggleAccordionItem = (index: number) => {
+  const toggleAccordionItem = (index:number) => {
     if(activeIndex === index) {
         setActiveIndex(null);
     } else {
@@ -33,15 +26,23 @@ const Accordion = () => {
 
   return (
     <div className = "flex flex-col gap-2">
-        {accordionItems.map((item, index) => {
+        {items.map((item, index) => {
+            const triggerId = `${generatedId}-trigger-${index}`
+            const contentId = `${generatedId}-content-${index}`
+    
             return (
             <div 
-             key = {index}
-             onClick = {() => toggleAccordionItem(index)}
-             className = "flex flex-col justify-between py-2 cursor-pointer border-b border-gray-200"
+             key = {triggerId}
+             className = "flex flex-col justify-between py-2 border-b border-gray-200"
              >
-                <button className = "flex flex-row justify-between py-1 focus-visible:rounded-md">
-                    <p className = "text-gray-700 font-medium">{item.title}</p>
+                <button 
+                aria-controls = {contentId}
+                id = {triggerId}
+                aria-expanded = {activeIndex === index ? true : false   }
+                className = "flex flex-row justify-between py-1 focus-visible:rounded-md cursor-pointer"
+                onClick = {() => toggleAccordionItem(index)}
+                >
+                    <p className = "text-gray-700 font-medium">{item.name}</p>
                     <MotionChevronLeft
                         animate = {{rotate: activeIndex === index ? -90 : 0}}
                         transition = {{ease: 'easeOut', duration: 0.2}}
@@ -52,15 +53,20 @@ const Accordion = () => {
                 <AnimatePresence>
                     {activeIndex === index && (
                     <MotionDiv
+                    role="region"
+                     id = {contentId}
+                     aria-labelledby= {triggerId}   
                      initial = {{height: 0}}
                      animate = {{height: 'auto'}}
                      exit={{ height: 0 }}
                      transition={{ duration: 0.2, ease: "easeOut" }}
                      className = "overflow-hidden"
                      >
-                        <p className = "text-gray-600">
+                        <div className = "py-2">
+                            <p className = "text-gray-600">
                             {item.content}
-                        </p>
+                            </p>
+                        </div>
                     </MotionDiv>
                 )}
                 </AnimatePresence>

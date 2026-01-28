801f869ab164b96de089f899134ba54d7f0859f6
mikocst <mikocastillo7@gmail.com>
2026-01-27 21:28:08 -0600

    Wired context into carousel component

--- src/components/Library/Carousel/Carousel.tsx
+++ src/components/Library/Carousel/Carousel.tsx
@@ -2,25 +2,49 @@ import Button from "@components/Button";
 import { buttonVariant } from "@types";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import CarouselContent from "./CarouselContent";
-import CarouselItem from "./CarouselItem";
+import { useState } from "react";
+import React from "react";
+import { CarouselContext } from "./CarouselContext";
 
-const Carousel = () => {
+interface CarouselProps {
+    children: React.ReactNode
+}
+
+const Carousel = ({children}: CarouselProps) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleLeftArrow = () => {
+        setActiveIndex(Math.max(activeIndex - 1, 0))
+        
+  }
+
+  const handleRightArrow = () => {
+        setActiveIndex(Math.min(activeIndex + 1, totalItems - 1))
+        
+  }
+
+  const totalItems = React.Children.count(children);
+
   return (
-    <div className = "flex justify-center items-center h-full relative">
+    <CarouselContext value = {{activeIndex, setActiveIndex, totalItems, itemWidth: 480}}>
+        <div className = "flex justify-center items-center h-full relative">
         <Button
         variant = {buttonVariant.Icon}
+        onClick={() => handleLeftArrow()}
         >
             <ArrowLeft className = "h-4 w-4"></ArrowLeft>
         </Button>
         <CarouselContent>
-            <CarouselItem></CarouselItem>
+            {children}
         </CarouselContent>
         <Button
         variant = {buttonVariant.Icon}
+        onClick={() => handleRightArrow()}
         >
             <ArrowRight className = "h-4 w-4"></ArrowRight>
         </Button>
     </div>
+    </CarouselContext>
   )
 }
 

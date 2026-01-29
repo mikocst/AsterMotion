b63b87838173b9dd0ccdd8233289bb411960893a
mikocst <mikocastillo7@gmail.com>
2026-01-29 16:18:22 -0600

    added transition and duration to carouselItem

--- src/components/Library/Carousel/CarouselItem.tsx
+++ src/components/Library/Carousel/CarouselItem.tsx
@@ -25,6 +25,7 @@ const CarouselItem = ({item, id}: CarouselItemProps) => {
     <motion.div 
     id = {itemId}
     animate = {myIndex === context.activeIndex ? {filter: 'blur(0px)'} : {filter: 'blur(10px)'}}
+    transition = {{ease: 'easeOut', duration: 0.2}}
     style={{width: context.itemWidth, minWidth: context.itemWidth}}
     className = "flex flex-shrink-0 h-full justify-center items-center rounded-md border border-gray-300 p-24">
             <p>{item}</p>

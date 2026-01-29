71d2dd0a89003a7d4d361ab0e15af7633ac2a130
mikocst <mikocastillo7@gmail.com>
2026-01-28 23:45:17 -0600

    set up motion div for animation

--- src/components/Library/Carousel/CarouselContent.tsx
+++ src/components/Library/Carousel/CarouselContent.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useContext } from 'react';
+import { CarouselContext } from './CarouselContext';
+import { motion } from 'motion/react';
 
 interface CarouselContentProps {
     children: React.ReactNode
@@ -6,13 +9,20 @@ interface CarouselContentProps {
 }
 
 const CarouselContent = ({children, ref}: CarouselContentProps) => {
+
+  const context = useContext(CarouselContext)
+  if(!context) return null;
+
   return (
     <div 
     ref = {ref}
     className = "h-full w-full overflow-hidden">
-        <div className = "flex flex-row">
+        <motion.div 
+        animate = {{x: -(context.activeIndex * context.itemWidth)}}
+        transition = {{ease: 'easeOut', duration: 0.2}}
+        className = "flex flex-row flex-nowrap w-max">
             {children}
-        </div>
+        </motion.div>
     </div>
   )
 }

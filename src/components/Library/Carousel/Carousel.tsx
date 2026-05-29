8cf1d117e0271ecd95c00755d865a3a168bc139b
mikocst <mikocastillo7@gmail.com>
2026-05-28 21:43:15 -0500

    optimized carousel component for deployment

--- src/components/Library/Carousel/Carousel.tsx
+++ src/components/Library/Carousel/Carousel.tsx
@@ -2,7 +2,7 @@ import Button from "@components/Button";
 import { buttonVariant } from "@types";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import CarouselContent from "./CarouselContent";
-import { useState, useRef, useEffect, useCallback } from "react";
+import { useState, useRef, useEffect, useCallback, useMemo } from "react";
 import React from "react";
 import { CarouselContext } from "./CarouselContext";
 
@@ -24,43 +24,54 @@ const Carousel = ({children}: CarouselProps) => {
         setActiveIndex(Math.min(activeIndex + 1, totalItems - 1)) 
   }
 
-  const totalItems = itemIds.length
+  const totalItems = itemIds.length;
+
+   const registerItem = useCallback((newItem: string) => {
+    setItemIds(prev => {
+        if(!prev.includes(newItem)) {
+            return [...prev, newItem]   
+        }
+
+        else {
+            return prev
+        }
+    })
+  },[]);
+
+  const contextValues = useMemo(() => {
+        return {
+            activeIndex,
+            setActiveIndex,
+            registerItem,
+            itemWidth,
+            itemIds,
+        }
+  }, [activeIndex, setActiveIndex, registerItem, itemWidth, itemIds,])
 
   useEffect(() => {
         const observer = new ResizeObserver((entries) => {      
         const newWidth = entries[0].contentRect.width;
         if (newWidth > 0 && newWidth < 5000) {
             setItemWidth(newWidth);
-        }
-            
+            }
         })
-        observer.observe(widthRef.current)
+        if(widthRef.current){
+            observer.observe(widthRef.current)
+        }
         return () => observer.disconnect()
   },[])
 
-  const registerItem = useCallback((newItem: string) => {
-    setItemIds(prev => {
-        if(!prev.includes(newItem)) {
-            return [...prev, newItem]   
-        }
-
-        else if (prev.includes(newItem)) {
-            return prev
-        }
-    })
-  },[])
-
   return (
-    <CarouselContext value = {{activeIndex, setActiveIndex, itemWidth, registerItem, itemIds}}>
+    <CarouselContext value = {contextValues}>
         <div className = "grid grid-cols-[auto_1fr_auto] gap-2 justify-center items-center w-full h-full relative overflow-hidden"
         >
         <Button
         variant = {buttonVariant.Icon}
         onClick={handleLeftArrow}
         >
-            <ArrowLeft className = "h-4 w-4 text-gray-600"></ArrowLeft>
+            <ArrowLeft className = "w-4 h-4 text-gray-600"></ArrowLeft>
         </Button>
-             <div ref = {widthRef} className = " overflow-hidden h-full">
+             <div ref = {widthRef} className = "h-full overflow-hidden ">
             <CarouselContent
             >
             {children}
@@ -70,7 +81,7 @@ const Carousel = ({children}: CarouselProps) => {
         variant = {buttonVariant.Icon}
         onClick={handleRightArrow}
         >
-            <ArrowRight className = "h-4 w-4 text-gray-600"></ArrowRight>
+            <ArrowRight className = "w-4 h-4 text-gray-600"></ArrowRight>
         </Button>
     </div>
     </CarouselContext>

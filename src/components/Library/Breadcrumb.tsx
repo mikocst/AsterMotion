0150a6dd676fd832e150212af99cc917afeaa763
mikocst <mikocastillo7@gmail.com>
2026-06-05 22:32:13 -0500

    logic for slicing items array implemented

--- src/components/Library/Breadcrumb.tsx
+++ src/components/Library/Breadcrumb.tsx
@@ -13,9 +13,28 @@ interface BreadCrumbProps {
     renderItem?:(item:BreadcrumbItem) => React.ReactNode
 }
 
-const Breadcrumb = () => {
+const Breadcrumb = ({items, maxItems = 5, separator, renderItem} : BreadCrumbProps) => {
+
+    const enforcedMaxItems = Math.max(maxItems, 3);
+    const listItemCount = enforcedMaxItems - 2;
+
+    let visibleStart: BreadcrumbItem[] = [];
+    let elipsesItems: BreadcrumbItem[] = [];
+    let visibleEnd: BreadcrumbItem[] = [];
+    
+    if(items.length > maxItems) {
+      visibleStart = items.slice(0, 1);
+      visibleEnd = items.slice(items.length - maxItems, items.length);
+      elipsesItems = items.slice(1, items.length - listItemCount)
+    }
+
+
   return (
-    <div>Breadcrumb</div>
+    <nav>
+      <ul>
+        <li>hi</li>
+      </ul>
+    </nav>
   )
 }
 

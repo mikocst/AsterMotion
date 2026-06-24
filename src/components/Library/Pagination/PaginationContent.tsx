5a2b0dd94da929b0448c1662faa80bedac1ccd53
mikocst <mikocastillo7@gmail.com>
2026-06-24 15:06:05 -0500

    functionanlity and accessibility implemented in paginationContent component

--- src/components/Library/Pagination/PaginationContent.tsx
+++ src/components/Library/Pagination/PaginationContent.tsx
@@ -3,19 +3,26 @@ import { Ellipsis } from "lucide-react"
 
 const PaginationContent = () => {
 
-    const {pagesToRender} = usePagination()
+    const {pagesToRender, activePage, onPageChange} = usePagination()
 
   return (
     <ul>
         {pagesToRender.map((item, index) => {
-            if (item === 'ellipsis'){
+            const isActive = item === activePage;
+            
+            if (typeof item === 'string'){
                 return (
                     <li key = {`ellipsis-${index}`}><Ellipsis/></li>
                 )
             }
             return (
                 <li key = {item}>
-                    <button className = "text-sm font-medium border rounded-md h-9 w-9">
+                    <button
+                    className = {`text-sm font-medium border rounded-md h-9 w-9 ${
+                                isActive ? "text-white bg-gray-500" : ""}`}
+                    aria-current = {isActive ? "page" : undefined}
+                    onClick = {() => onPageChange(item)}
+                    >
                         {item}
                     </button>
                 </li>

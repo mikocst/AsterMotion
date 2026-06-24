d910514d85df3232d6ad0a7c62cbecd8e4300094
mikocst <mikocastillo7@gmail.com>
2026-06-24 12:53:04 -0500

    Functions for pagination range implemented

--- src/components/Library/Pagination/PaginationContext.tsx
+++ src/components/Library/Pagination/PaginationContext.tsx
@@ -6,6 +6,11 @@ interface PaginationContextValue {
     onPageChange: (page:number) => void
 }
 
+const range = (start: number, end:number): number[] => {
+    const length = end - start + 1;
+    return Array.from({length}, (_, idx) => start + idx)
+}
+
 export const generatePaginationRange = (
     activePage: number,
     totalPages: number,
@@ -13,17 +18,33 @@ export const generatePaginationRange = (
 ) : (number | string)[] => {
     
     if (totalPages <= maxButtons) {
-
+        return(
+           range(1, totalPages)
+        )
     }
 
     const showLeftEllipsis = activePage > maxButtons - 1;
     const showRightEllipsis = activePage < totalPages - (maxButtons -2);
 
     if(!showLeftEllipsis && showRightEllipsis){
-
+        const leftRange = range(1, maxButtons - 1)
+        return (
+            [...leftRange, 'ellipsis', totalPages]
+        )
     }
 
     if (showLeftEllipsis && !showRightEllipsis){
-        
+        const rightRange = range(totalPages - maxButtons, totalPages)
+
+        return(
+            [1, 'ellipsis', ...rightRange]
+        )
     }
+
+    if(showLeftEllipsis && showRightEllipsis){
+        const middleRange = range(activePage - 1, activePage + 1);
+        return [1, 'ellipsis', ...middleRange, 'ellipsis', totalPages]
+    }
+
+    return []
 }

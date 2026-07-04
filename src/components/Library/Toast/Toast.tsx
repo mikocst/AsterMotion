b7eda4db6c6603cab1fef1cbd5fe5223ba4bd4bb
Miko Castillo <mikocastillo7@gmail.com>
2026-07-04 00:50:40 -0400

    Record created

--- src/components/Library/Toast/Toast.tsx
+++ src/components/Library/Toast/Toast.tsx
@@ -1,13 +1,59 @@
 import React from 'react'
+import { CheckCircle, Info, TriangleAlert, CircleAlert } from 'lucide-react'
 
 interface ToastProps {
-    type: "default" | "description" | "success" | "info" | "warning" | "error" | "custom"
-    
+    toastType: "default" | "description" | "success" | "info" | "warning" | "error" | "custom"
+    description: string
+    id: string
 }
 
-const Toast = () => {
+interface ToastVariants {
+    styles: string,
+    icon?: React.ReactNode
+    headerRequired?: boolean
+}
+
+const toastMap : Record<ToastProps['toastType'], ToastVariants> = {
+    default: {
+        styles: "border-gray-200 text-gray-500",
+    },
+    description: {
+        styles: "border-gray-200 text-gray-500",
+        headerRequired: true
+    } ,
+    success: {
+        styles: "bg-green-50 border-green-200 text-green-800",
+        icon: <CheckCircle className="h-5 w-5" />,
+        headerRequired: false
+    } ,
+    info: {
+        styles: "border-gray-200 text-gray-500",
+        icon: <Info className = "h-5 w-5 text-gray-700"/>
+
+    } ,
+    warning: {
+        styles: "border-gray-200 text-yellow-700",
+        icon: <TriangleAlert className = "h-5 w-5"/>
+    } ,
+    error: {
+        styles: "border-gray-200 text-red-700",
+        icon: <CircleAlert className = "h-5 w-5"/>
+    } ,
+    custom: {
+        styles: "border-gray-200 text-gray-500",
+    } ,
+
+}
+
+
+const Toast = ({toastType, description, id} : ToastProps) => {
+
+  const variants = toastMap[toastType];
+
   return (
-    <div>Toast</div>
+    <div className = "flex flex-col gap-1">
+        <p className = "text-gray-500">{description}</p>
+    </div>
   )
 }
 
--- src/components/Library/Toast/Toaster.tsx
+++ src/components/Library/Toast/Toaster.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 interface ToasterProps {
     position: "top-left" | "top-right" | "top-center" | "bottom-left" | "bottom-right" | "bottom-center"
+    onClick: () => void
+    visibleToast: boolean
 }
 
 const Toaster = () => {

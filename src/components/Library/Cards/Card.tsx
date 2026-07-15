7e13a0b6056e2ddcee59413b6d7723528e6cf49a
mikocst <mikocastillo7@gmail.com>
2026-07-14 23:29:29 -0500

    Created style map and variables for users to call defined sizing styles

--- src/components/Library/Cards/Card.tsx
+++ src/components/Library/Cards/Card.tsx
@@ -2,11 +2,36 @@
 
 interface CardProps {
   children: React.ReactNode
+  size: "sm" | "md" | "lg" | "xl"
 }
 
-const Card = ({children} : CardProps) => {
+interface CardCSSProperties extends React.CSSProperties {
+  "--card-px": string
+  "--card-py": string
+}
+
+const styleMap: Record<CardProps["size"], CardCSSProperties> = {
+  sm: {
+        "--card-px": "1rem",
+        "--card-py": "1rem",
+      },
+  md: {
+      "--card-px": "1.5rem",
+      "--card-py": "1.5rem",
+      },
+  lg: {
+      "--card-px": "2rem",
+      "--card-py": "2rem",
+    },
+  xl: {
+    "--card-px": "2.5rem",
+    "--card-py": "2.5rem",
+    }
+}
+
+const Card = ({children, size = "md"} : CardProps) => {
   return (
-    <div>
+    <div style = {styleMap[size]}>
       {children}
     </div>
   )
